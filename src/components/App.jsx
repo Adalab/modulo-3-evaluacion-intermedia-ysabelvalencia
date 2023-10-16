8513746662019cb0aea004eff2566f2562441666
ysabelvalencia <ysavalencia@hotmail.com>
2023-10-16 09:18:33 +0200

    basic html

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,6 +11,38 @@ function App() {
 
   return (
   <>
+  <header>
+    <h1>Country Info App</h1>
+    <p>Explore information about countries, capitals, and flags. Add new countries and filter through the list!</p>
+  </header>
+  <main>
+    <section>
+      <form>
+        <h2>Filters</h2>
+        <div>
+          <p>By country</p>
+          <input type="text" />
+          <p>By Continent</p>
+          <select name="" id="">
+          <option value="All">All</option>
+          <option value="Africa">Africa</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="North America">North America</option>
+          <option value="Oceania">Oceania</option>
+          <option value="South America">South America</option>
+          </select>
+        </div>
+      </form>
+    </section>
+
+    <section className='all-countries'>
+      <ul className='all-countries-ul'>
+        AQUI VAN LOS PAISES
+      
+      </ul>
+    </section>
+  </main>
 
   </>
     
